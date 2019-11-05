5575dda834c961b8c45966586a5c2083e8935cb5
scotoma <506826727@qq.com>
2019-11-05 15:03:10 +0800

    v1.4

---
+++ src/main/java/com/atguigu/cache/config/MyRedisConfig.java
@@ -0,0 +1,24 @@
+package com.atguigu.cache.config;
+
+import com.atguigu.cache.bean.Employee;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+
+import java.net.UnknownHostException;
+
+@Configuration
+public class MyRedisConfig {
+    @Bean
+    public RedisTemplate<Object, Employee> empRedisTemplate(RedisConnectionFactory redisConnectionFactory) throws UnknownHostException {
+        RedisTemplate<Object, Employee> template = new RedisTemplate();
+        template.setConnectionFactory(redisConnectionFactory);
+        Jackson2JsonRedisSerializer<Employee> jackson2JsonRedisSerializer = new Jackson2JsonRedisSerializer<Employee>(Employee.class);
+        template.setDefaultSerializer(jackson2JsonRedisSerializer);
+        return template;
+    }
+
+}
--- src/test/java/com/atguigu/cache/Spingboot01CacheApplicationTests.java
+++ src/test/java/com/atguigu/cache/Spingboot01CacheApplicationTests.java
@@ -5,17 +5,43 @@ import com.atguigu.cache.mapper.EmployeeMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
 
 @SpringBootTest
 class Spingboot01CacheApplicationTests {
     @Autowired
     EmployeeMapper employeeMapper;
 
+    @Autowired
+    StringRedisTemplate stringRedisTemplate;  //操作k-v都是字符串
+
+    @Autowired
+    RedisTemplate redisTemplate;//k-v都是对象
+
+    @Autowired
+    RedisTemplate<Object,Employee> empRedisTemplate;
+
+    @Test
+    public void test01(){
+        stringRedisTemplate.opsForValue().append("name","张三");
+    }
+
+    @Test
+    public void test02(){
+        Employee employee=employeeMapper.getEmpById(1);
+        System.out.println(employee);
+        redisTemplate.opsForValue().set("emp01",employee);
+        //将数据以json的方式保存
+        //手动将对象转为json
+        //redisTemplate 默认的序列化规则；改编默认的序列化规则
+        //empRedisTemplate.opsForValue().set("emp-01",employee);
+    }
+
     @Test
     void contextLoads() {
         Employee employee=employeeMapper.getEmpById(1);
         System.out.println(employee);
-
     }
 
 }
